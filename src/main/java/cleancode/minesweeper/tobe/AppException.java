9d603f2a4d877751e694bf0d16c5155eb917a7e6
khs96523 <khs96523@gmail.com>
2024-10-03 21:26:03 +0900

    REFACTOR: Extract Class With SRP

--- src/main/java/cleancode/minesweeper/tobe/AppException.java
+++
@@ -1,7 +0,0 @@
-package cleancode.minesweeper.tobe;
-
-public class AppException extends RuntimeException {
-    AppException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/cleancode/minesweeper/tobe/GameException.java
@@ -0,0 +1,7 @@
+package cleancode.minesweeper.tobe;
+
+public class GameException extends RuntimeException {
+    GameException(String message) {
+        super(message);
+    }
+}
