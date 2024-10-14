6cc5b92cace8ff5429790740f1a770b324b11bcd
khs96523 <khs96523@gmail.com>
2024-10-14 15:41:40 +0900

    REFACTOR: 객체에 메세지 보내기

--- src/main/java/cleancode/studycafe/tobe/model/StudyCafePass.java
+++ src/main/java/cleancode/studycafe/tobe/model/StudyCafePass.java
@@ -1,8 +1,10 @@
 package cleancode.studycafe.tobe.model;
 
+import java.util.Set;
+
 public class StudyCafePass {
 
-    private final StudyCafePassType passType;
+    public final StudyCafePassType passType;
     private final int duration;
     private final int price;
     private final double discountRate;
@@ -16,6 +18,10 @@ public class StudyCafePass {
 
     public static StudyCafePass of(StudyCafePassType passType, int duration, int price, double discountRate) {
         return new StudyCafePass(passType, duration, price, discountRate);
+    }
+
+    public boolean isSamePassType(StudyCafePassType passType) {
+        return this.passType == passType;
     }
 
     public StudyCafePassType getPassType() {
@@ -47,4 +53,12 @@ public class StudyCafePass {
         return "";
     }
 
+    public boolean isSameDurationType(StudyCafeLockerPass lockerPass) {
+        return lockerPass.isSamePassType(this.passType)
+                && lockerPass.isSameDuration(this.duration);
+    }
+
+    public boolean cannotUseLocker() {
+        return this.passType.isNotLockerType();
+    }
 }
